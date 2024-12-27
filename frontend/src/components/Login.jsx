ddfd055bcc0b4ab2a2be9b7df905c080877a40ab
Tashrif-007 <tashrifpro@gmail.com>
2024-12-28 00:45:12 +0600

    integrated login and signup,login routes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,13 +2,12 @@ import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-const SignUp = () => {
-  const navigate = useNavigate();
+const Login = () => {
+  //const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: '',
-    name: '',
     password: '',
   });
   const [error,setError] = useState(null);
@@ -23,31 +22,36 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null)
 
     try {
-      const response = await fetch("http://localhost:3500/auth/register", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+        const response = await fetch("http://localhost:3500/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
 
-      if(!response.ok) {
-        throw new Error("Registration failed");
-      }
-      alert("reg done")
-      setFormData({
-        email: '',
-        name: '',
-        password: '',
-      })
-      setError(null);
-      navigate("/login");
+        if(!response.ok) {
+            const errData = await response.json();
+            throw new Error(errData.message || "Failed to login");
+        }
 
+        const data = await response.json();
+        const {token,user} = data;
+
+        localStorage.setItem('token', token);
+        console.log(user);
+        alert("Login Successful");
+        setFormData({
+            email: "",
+            password: "",
+        });
+        
     } catch (error) {
-      console.error(error.message);
-      setError(error.message);
+        console.error(error.message);
+        setError(error.message);
     }
   }
 
@@ -62,14 +66,13 @@ const SignUp = () => {
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <form className='flex flex-col gap-4 glassStyle w-[20em] border-4 border-blue-500 rounded p-4' onSubmit={handleSubmit}>
-          <h1 className='text-center text-blue-700 text-[32px]'>SignUp</h1>
+          <h1 className='text-center text-blue-700 text-[32px]'>Login</h1>
           <TextField id='outlined-basic' label='Email' variant='outlined' onChange={handleChange} type='email' name='email' value={formData.email}/>
-          <TextField id='outlined-basic' label='Username' variant='outlined' onChange={handleChange} type='text' name='name' value={formData.name}/>
           <TextField id='outlined-basic' label='Password' variant='outlined' onChange={handleChange} type='password' name='password' value={formData.password}/>
           {error && <p className='text-red-500 text-center'>{error}</p>}
-          <ColorButton variant='contained' size='large' type='submit'>SignUp</ColorButton>
+          <ColorButton variant='contained' size='large' type='submit'>Login</ColorButton>
           <div className='flex justify-between'>
-            <p>Already have account?</p>
+            <p>Don&apos;t have an account?</p>
             {/* <Link to='/login' className='text-blue-700'>Login</Link> */}
           </div>
       </form>
@@ -77,4 +80,4 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+export default Login
