2191a39760f3ab27024d4c0d8bee7ab6d85e9e62
Tashrif-007 <tashrifpro@gmail.com>
2024-12-29 22:29:08 +0600

    frontend posts creation and postcard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Home from './components/Home';
+import Posts from './pages/Posts';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path='/' element={<Home />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path='/posts' element={<Posts />}/>
         </Routes>
       </div>
     </Router>
