b489d5027d211bd05abbc009890a36cc9da64323
Tashrif-007 <tashrifpro@gmail.com>
2025-01-07 22:11:49 +0600

    added application table in schema

--- backend/prisma/migrations/20250107151025_add_user_type/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE `Company` MODIFY `description` VARCHAR(191) NULL,
-    MODIFY `website` VARCHAR(191) NULL,
-    MODIFY `techStack` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `JobSeeker` MODIFY `salaryExpectation` DOUBLE NULL,
-    MODIFY `location` VARCHAR(191) NULL,
-    MODIFY `experience` INTEGER NULL;
