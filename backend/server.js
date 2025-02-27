81c96ce58760f7aab138a78090539cc652cbbc59
Tashrif-007 <tashrifpro@gmail.com>
2025-02-28 00:07:28 +0600

    socket initialized

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ import router from './routes/auth.routes.js';
 import postRouter from './routes/post.routes.js';
 import applyRouter from './routes/apply.routes.js';
 import recRouter from './routes/recommend.routes.js';
+import {app,server} from "./socket/socket.js";
+// const app = express();
 
-const app = express();
 const PORT = process.env.PORT || 3500;
 dotenv.config();
 app.use(express.json());
@@ -19,6 +20,10 @@ app.use("/post", postRouter);
 app.use("/apply", applyRouter);
 app.use("/rec",recRouter);
 
-app.listen(PORT, () => {
+app.get("*", (req,res) => {
+    res.send("JobNode App!");
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
