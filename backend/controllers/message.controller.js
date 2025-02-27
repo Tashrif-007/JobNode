47a01d1ba230eeb72d8ce79d018168d7a182898e
Tashrif-007 <tashrifpro@gmail.com>
2025-02-28 01:19:05 +0600

    getMessagesAPI

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -63,3 +63,42 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 };
+
+export const getMessages = async (req,res) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if(!token) {
+            return res.status(401).json({message: "Unauthorized access"});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const senderId = decoded.userId;
+
+        const {id: receiverId } = req.params;
+        const numReceiver = Number(receiverId);
+
+        const conversation = await prisma.conversation.findFirst({
+            where: {
+                OR: [
+                    { user1: senderId, user2: numReceiver },
+                    { user1: numReceiver, user2: senderId }
+                ]
+            }
+        });
+
+        if(!conversation) {
+            return res.status(404).json({ error: "Conversation not found" });
+        }
+
+        const messages = await prisma.Message.findMany({
+            where: { conversationId: conversation.id},
+            orderBy: { createdAt: "asc" }
+        });
+
+        return res.status(200).json(messages);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}
